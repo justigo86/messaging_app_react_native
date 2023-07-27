61fa14e9e731166a49fa38e3b592ed3cf9fd0b77
Otto <justigo86@gmail.com>
2023-07-26 23:15:53 -0400

    started outline for messageFeedItem

--- src/components/MessageFeedItem/messageFeedItem.component.tsx
+++ src/components/MessageFeedItem/messageFeedItem.component.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 const MessageFeedItem = () => {
-  return <Text>MessageFeedItem</Text>;
+  return (
+    <View>
+      <Image
+        source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg' }}
+        style={{ width: 50, height: 50 }}
+      ></Image>
+      <View>
+        <View>
+          <Text>Name</Text>
+          <Text>Ti:me</Text>
+        </View>
+        <Text>Test text</Text>
+      </View>
+    </View>
+  );
 };
 
 export default MessageFeedItem;
