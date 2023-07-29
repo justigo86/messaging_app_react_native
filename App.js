11b71bfa8353842f8e0b6088c63992733a269c50
Otto <justigo86@gmail.com>
2023-07-29 15:10:59 -0400

    added dummy data and props to App.js for messageFeedItem

--- App.js
+++ App.js
@@ -2,11 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MessageFeedItem from './src/components/MessageFeedItem/messageFeedItem.component';
 
+const chatDummyData = {
+  id: '1',
+  user: {
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg',
+    name: 'Lukas',
+  },
+  mostRecentMessage: {
+    text: 'Oke',
+    createdAt: '07:30',
+  },
+};
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <MessageFeedItem />
-      <MessageFeedItem />
+      <MessageFeedItem chat={chatDummyData} />
       <StatusBar style="auto" />
     </View>
   );
--- src/components/MessageFeedItem/messageFeedItem.component.tsx
+++ src/components/MessageFeedItem/messageFeedItem.component.tsx
@@ -2,22 +2,19 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import styles from './messageFeedItem.styles';
 
-const MessageFeedItem = () => {
+const MessageFeedItem = ({ chat }) => {
   return (
     <View style={styles.container}>
-      <Image
-        source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg' }}
-        style={styles.avatar}
-      ></Image>
+      <Image source={{ uri: chat.user.image }} style={styles.avatar}></Image>
       <View style={styles.messageContent}>
         <View style={styles.row}>
           <Text style={styles.username} numberOfLines={1}>
-            Name
+            {chat.user.name}
           </Text>
-          <Text style={styles.time}>Ti:me</Text>
+          <Text style={styles.time}>{chat.mostRecentMessage.createdAt}</Text>
         </View>
         <Text style={styles.messageText} numberOfLines={2}>
-          Test text
+          {chat.mostRecentMessage.text}
         </Text>
       </View>
     </View>
