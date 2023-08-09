1e8e55e4a84ae29b9b90f85c55abcc73d1aae926
Otto <justigo86@gmail.com>
2023-08-08 22:27:58 -0400

    created backend with authentication in Amplify - installed AmplifyCLI - added my user info - updated App.tsx with awsconfig

--- App.tsx
+++ App.tsx
@@ -4,6 +4,11 @@ import { View } from 'react-native';
 import styles from './App.styles';
 import MessageChat from './src/pages/messageChat/messageChat';
 import Navigator from './src/navigation/navigator.component';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+
+Amplify.configure(awsconfig);
+// Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });  //if needed
 
 export default function App() {
   return (
