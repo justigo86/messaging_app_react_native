45640fc372d7c3c02902240693664942a5441bd3
Otto <justigo86@gmail.com>
2023-09-10 23:12:39 -0400

    updated messageChat to fetch chat data on page load

--- src/pages/messageChat/messageChat.page.tsx
+++ src/pages/messageChat/messageChat.page.tsx
@@ -1,22 +1,74 @@
-import React, { useEffect } from 'react';
-import { ImageBackground, FlatList, KeyboardAvoidingView, Platform } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  ImageBackground,
+  FlatList,
+  KeyboardAvoidingView,
+  Platform,
+  ActivityIndicator,
+} from 'react-native';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import bg from '../../../assets/images/BG.png';
 import styles from './messageChat.styles';
 import messages from '../../../assets/data/messages.json';
 import Message from '../../components/message/message.component';
 import InputBox from '../../components/inputBox/inputBox.component';
+import { API, graphqlOperation } from 'aws-amplify';
+import { getUser, messageChatUsersByUserId } from '../../graphql/queries';
+
+type MessageChatByUserID = {
+  messageChatUsersByUserId: {
+    items: {
+      id: string;
+      userId: string;
+      messageChatId: string;
+      user: {
+        id: string;
+        name: string;
+        image: string;
+        status: string;
+        createdAt: string;
+        updatedAt: string;
+      };
+      messageChat: {
+        id: string;
+        createdAt: string;
+        updatedAt: string;
+        messageChatMostRecentMessageId: string;
+      };
+      createdAt: string;
+      updatedAt: string;
+    };
+    nextToken: string;
+    startedAt: string;
+  };
+};
 
 const MessageChat = ({ route }) => {
   // const route = useRoute();
   const navigation = useNavigation();
   const messageChatID = route.params.id;
+  const [chat, setChat] = useState(null);
 
   // console.log(route);
 
   useEffect(() => {
+    const fetchChats = async () => {
+      const chatData = (await API.graphql(
+        graphqlOperation(messageChatUsersByUserId, { chatId: route.params.id })
+      )) as { data: MessageChatByUserID };
+
+      setChat(chatData.data.messageChatUsersByUserId.items);
+    };
+    fetchChats();
+  }, []);
+
+  useEffect(() => {
     navigation.setOptions({ title: route.params.name });
   }, [route.params.name]);
+
+  if (!chat) {
+    return <ActivityIndicator />;
+  }
 
   return (
     <KeyboardAvoidingView
